6cf66c1deb96d05f98807fb187b674dfa5d96a8f
Linh Vu <vuhoanglinh2002@gmail.com>
2019-10-05 02:25:23 +0700

    finish pc mode

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,14 +1,15 @@
 jQuery(document).ready(function($) {
 
-    $('.list-group [data-toggle="collapse"]').on('click touchstart', function() {
-        let id = $(this).attr('href');
-        $(this).parent('.list-group').find('[data-toggle="collapse"]').each(function() {
-          let nextId = $(this).attr('href');
-          if (id != nextId) {
-            $(nextId).removeClass('show');
-            $(this).addClass('collapsed');
-          }
-        });
-    });
+  $('.list-group [data-toggle="collapse"]').on('click touchstart', function() {
+      let id = $(this).attr('href');
+      $(this).addClass('active');
+      $(this).parent('.list-group').find('[data-toggle="collapse"]').each(function() {
+        let nextId = $(this).attr('href');
+        if (id != nextId) {
+          $(nextId).removeClass('show');
+          $(this).removeClass('active');
+        }
+      });
+  });
 
 });
