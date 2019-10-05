5e93f1d078165a97429202fe732f002c71d41db3
Linh Vu <vuhoanglinh2002@gmail.com>
2019-10-05 14:52:09 +0700

    add search box

--- dist/js/app.js
+++ dist/js/app.js
@@ -12,4 +12,28 @@ jQuery(document).ready(function($) {
       });
   });
 
+  $('[aria-controls="modal-search"]').on('click touchstart', function() {
+    $('#modal-search').addClass('modal--is-visible');
+  });
+
+  $('.modal__close-btn').on('click touchstart', function() {
+    $('#modal-search').removeClass('modal--is-visible');
+  });
+
+  // $(document).mouseup(function(e) {
+  //   let container = $('.full-screen-search');
+
+  //   // if the target of the click isn't the container nor a descendant of the container
+  //   if (!container.is(e.target) && container.has(e.target).length === 0) 
+  //   {
+  //       container.parent('#modal-search').removeClass('modal--is-visible');
+  //   }
+  // });
+
+  // $('.carousel').on('slid.bs.carousel', function() {
+  //   $("ol.carousel-indicators li").removeClass("active");
+  //   let indicators = $("ol.carousel-indicators li.active").data("slide-to");
+  //   $("ol.carousel-indicators").find("[data-slide-to='" + indicators + "']").addClass("active");
+  // })
+
 });
