10d0afe73e4a8e123d9e5ad50e82ec592a29cf04
Linh Vu <vuhoanglinh2002@gmail.com>
2019-10-09 00:04:56 +0700

    add about page

--- dist/js/app.js
+++ dist/js/app.js
@@ -20,20 +20,9 @@ jQuery(document).ready(function($) {
     $('#modal-search').removeClass('modal--is-visible');
   });
 
-  // $(document).mouseup(function(e) {
-  //   let container = $('.full-screen-search');
-
-  //   // if the target of the click isn't the container nor a descendant of the container
-  //   if (!container.is(e.target) && container.has(e.target).length === 0) 
-  //   {
-  //       container.parent('#modal-search').removeClass('modal--is-visible');
-  //   }
-  // });
-
-  // $('.carousel').on('slid.bs.carousel', function() {
-  //   $("ol.carousel-indicators li").removeClass("active");
-  //   let indicators = $("ol.carousel-indicators li.active").data("slide-to");
-  //   $("ol.carousel-indicators").find("[data-slide-to='" + indicators + "']").addClass("active");
-  // })
+  $('#introduction-tab a').on('click touchstart', function() {
+    let slide = parseInt($(this).attr('data-slide-to'));
+    $('#carouselAbout').carousel(slide);
+  });
 
 });
